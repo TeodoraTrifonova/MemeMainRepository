346b35fb4b6f5e8039cce12b236c7b60f10677d0
TeodoraTrifonova <92271887+TeodoraTrifonova@users.noreply.github.com>
2022-08-08 11:59:24 +0300

    .

---
+++ projectspring/MemeCrudProject/src/main/java/com/tinqin/memeproject/memecrudproject/MemeCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tinqin.memeproject.memecrudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeCrudProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeCrudProjectApplication.class, args);
+    }
+
+}
---
+++ projectspring/MemeCrudProject/src/test/java/com/tinqin/memeproject/memecrudproject/MemeCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinqin.memeproject.memecrudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeCrudProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
